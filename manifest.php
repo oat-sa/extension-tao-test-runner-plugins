3115a827ad2eda01ef9a8720f8359ec4b5bf1c68
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-07-22 16:47:45 +0200

    initial import

---
+++ manifest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ */
+
+return array(
+    'name' => 'taoTestRunnerPlugins',
+    'label' => 'Manage test runner plugins',
+    'description' =>  "Manage test runner's plugins",
+    'license' => 'GPL-2.0',
+    'version' => '0.1.0',
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => array(
+        'tao' => '>=5.4.0',
+        'taoDelivery' =>  '>=4.2.0',
+        'taoDeliveryRdf' =>  '>=1.6.0',
+        'taoQtiTest' =>  '>=4.3.0',
+    ),
+    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoTestRunnerPluginsManager',
+    'acl' => array(
+        array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoTestRunnerPluginsManager', array('ext' => 'taoTestRunnerPlugins')),
+    ),
+    'update' => 'oat\\taoTestRunnerPlugins\\scripts\\update\\Updater',
+    'install' => [
+        'php' => [
+        ]
+    ],
+    'uninstall' => [
+        'php' => [
+        ]
+    ],
+    'routes' => array(
+        '/taoTestRunnerPlugins' => 'oat\\taoTestRunnerPlugins\\controller'
+    ),
+    'constants' => array(
+        # views directory
+        "DIR_VIEWS" => dirname(__FILE__) . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR,
+
+        #BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL . 'taoTestRunnerPlugins/',
+
+        #BASE WWW required by JS
+        'BASE_WWW' => ROOT_URL . 'taoTestRunnerPlugins/views/'
+    ),
+    'extra' => array(
+        'structures' => dirname(__FILE__) . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR . 'structures.xml',
+    )
+);
---
+++ views/templates/Delivery/index.tpl
@@ -0,0 +1,1 @@
+<h1>Plug Plug</h1>
