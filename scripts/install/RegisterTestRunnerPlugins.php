923ee65a10b1b1a8f34c18cde9165ee825be3056
Christophe Noël <christophe@taotesting.com>
2017-03-28 10:38:59 +0200

    bump version and install script template

---
+++ scripts/install/RegisterTestRunnerPlugins.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+namespace oat\taoTestRunnerPlugins\scripts\install;
+
+use oat\oatbox\extension\InstallAction;
+use common_report_Report as Report;
+use oat\taoTests\models\runner\plugins\PluginRegistry;
+use oat\taoTests\models\runner\plugins\TestPlugin;
+
+/**
+ * Install action that registers the test runner plugins
+ * !!! Do not reference this action directly in the manifest,
+ * but use a client extension to enable/disable plugins for a specific instance
+ *
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+class RegisterTestRunnerPlugins extends InstallAction
+{
+    public static $plugins = [
+        'probes' => [
+            [
+                'id' => 'latency',
+                'name' => 'Latency',
+                'module' => 'taoTestRunnerPlugins/runner/plugins/probes/latency',
+                'description' => 'Register metrics',
+                'category' => 'probes',
+                'active' => false,
+                'tags' => [ ]
+            ]
+        ],
+        'security' => [
+            [
+                'id' => 'autoPause',
+                'name' => 'Auto Pause',
+                'module' => 'taoTestRunnerPlugins/runner/plugins/security/autoPause',
+                'description' => 'Persist the pause state',
+                'category' => 'security',
+                'active' => false,
+                'tags' => [ ]
+            ], [
+                'id' => 'blurPause',
+                'name' => 'Blur Pause',
+                'module' => 'taoTestRunnerPlugins/runner/plugins/security/blurPause',
+                'description' => 'Pause the test when leaving the test window',
+                'category' => 'security',
+                'active' => false,
+                'tags' => [  ]
+            ], [
+                'id' => 'disableCommands',
+                'name' => 'Disable Commands',
+                'module' => 'taoTestRunnerPlugins/runner/plugins/security/disableCommands',
+                'description' => 'Disable and report some forbidden shortcuts',
+                'category' => 'security',
+                'active' => false,
+                'tags' => [  ]
+            ], [
+                'id' => 'preventCopy',
+                'name' => 'Prevent Copy',
+                'module' => 'taoTestRunnerPlugins/runner/plugins/security/preventCopy',
+                'description' => 'Prevent copying from CTRL-C/X/V shortcuts',
+                'category' => 'security',
+                'active' => false,
+                'tags' => [  ]
+            ], [
+                'id' => 'fullscreen',
+                'name' => 'Full Screen',
+                'module' => 'taoTestRunnerPlugins/runner/plugins/security/fullScreen',
+                'description' => 'Force the test in full screen mode',
+                'category' => 'security',
+                'active' => false,
+                'tags' => [  ]
+            ]
+        ]
+    ];
+
+    /**
+     * Run the install action
+     */
+    public function __invoke($params)
+    {
+
+        $registry = PluginRegistry::getRegistry();
+        $count = 0;
+
+        foreach(self::$plugins as $category => $categoryPlugins) {
+            foreach($categoryPlugins as $pluginData){
+                if( $registry->register(TestPlugin::fromArray($pluginData)) ) {
+                    $count++;
+                }
+            }
+        }
+
+        return new Report(Report::TYPE_SUCCESS, $count .  ' plugins registered.');
+    }
+}
