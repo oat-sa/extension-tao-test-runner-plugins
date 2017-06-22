56735614a75ed6537d036fc92662c05048f4cc1f
Christophe Noël <christophe@taotesting.com>
2017-06-22 11:49:38 +0200

    first working behavior, not on ie

--- views/js/runner/plugins/security/preventScreenshot.js
+++ views/js/runner/plugins/security/preventScreenshot.js
@@ -30,6 +30,57 @@ define([
     var platform = navigator.platform.indexOf('Mac') < 0 ? 'win' : 'mac';
 
     /**
+     * Sniff Internet Explorer which is the only browser to implement window.clipboardData
+     * @type {boolean}
+     */
+    var isIe = typeof window.clipboardData !== 'undefined';
+
+    /**
+     * On windows, this is what we will attempt to put in the clipboard on a PrintScreen event
+     */
+    var overrideContent = ' ';
+
+    function triggerCopyEvent() {
+        console.log('about to trigger copy event');
+        if (isIe) {
+            console.log('setting design mode');
+            document.designMode = 'on'; // required by IE to actually trigger the 'copy' event
+        }
+        if (document.queryCommandSupported('copy')) {
+            console.log('triggering copy event');
+            document.execCommand('copy');
+        }
+        if (isIe) {
+            document.designMode = 'off';
+        }
+    }
+
+    function handleCopyEvent(event) {
+        console.log('handling copy event');
+        overrideClipboard(event.clipboardData);
+        event.preventDefault();
+    }
+
+    function overrideClipboard(clipboardData) {
+        // var now = new Date(),
+        //     formattedDate = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+        console.log('overidding clipboard');
+        // IE specific
+        if (isIe) {
+            console.log('overriding in IE');
+            window.clipboardData.setData('Text', overrideContent);
+
+        // Normal browsers behavior
+        } else {
+            console.log('overriding in a normal browser');
+
+            clipboardData.setData('text/plain', overrideContent);
+            // clipboardData.setData('text/html', 'overriden html at ' + formattedDate); // todo: remove me?
+            // console.log(clipboardData.getData('text/plain'));
+        }
+    }
+
+    /**
      * Creates the plugin.
      * Prevents screenshots (mac) and pauses assessment on print screen (win)
      */
@@ -42,6 +93,20 @@ define([
          */
         init: function init() {
             // this function is mandatory
+            var testRunner = this.getTestRunner(),
+                areaBroker = testRunner.getAreaBroker(),
+                $toolbox = areaBroker.getToolboxArea(),
+                $trigger = $('<div>', {
+                    text: 'trigger for cut/paste'
+                });
+
+            // $toolbox.append($trigger);
+
+            // $trigger.on('click', function() {
+            //     fireCopy();
+            // });
+
+
         },
 
         /**
@@ -54,14 +119,14 @@ define([
             // For mac - blur on Cmd+Shift
             if (platform === 'mac') {
                 $(window)
-                .on('keydown' + '.' + this.name, function (e) {
+                .on('keydown' + '.' + this.getName(), function (e) {
                     if (e.metaKey && e.shiftKey) {
                         $('body').css('filter', 'blur(20px)');
                     }
                 })
                 // Note - When user hits Cmd+Shift+4, they must press any key
                 // to remove blur (that is not Cmd+Shift)
-                .on('keyup' + '.' + this.name, function (e) {
+                .on('keyup' + '.' + this.getName(), function (e) {
                     if (!e.metaKey || !e.shiftKey) {
                         $('body').css('filter', '');
                     }
@@ -70,25 +135,37 @@ define([
 
             // Windows - pause on PrtScn
             else if (platform === 'win') {
+                // try overriding any successful screenshot
+                console.log('listening to copy event');
+                // document.addEventListener('copy', handleCopyEvent);
+                document.addEventListener('copy', function(e) {
+                    console.log('COPY TRIGGERED !');
+                    e.preventDefault();
+                });
+
                 $(window)
-                .on('keyup' + '.' + this.name, function (e) {
+                .on('keyup' + '.' + this.getName(), function (e) {
                     if (e.key === 'PrintScreen') {
-                        testRunner
-                        .trigger('prohibited-key', 'PrintScreen')
-                        .trigger('pause', {
-                            message: __('The assessment has been paused due to an attempt to print screen. Please contact your proctor or administrator to resume your assessment.'),
-                            reasons: {
-                                category: __('examinee'),
-                                subCategory: __('behaviour')
-                            }
-                        });
+                        triggerCopyEvent();
+                        console.log('PrintScreen');
+
+                        // testRunner
+                        // .trigger('prohibited-key', 'PrintScreen')
+                        // .trigger('pause', {
+                        //     message: __('The assessment has been paused due to an attempt to print screen. Please contact your proctor or administrator to resume your assessment.'),
+                        //     reasons: {
+                        //         category: __('examinee'),
+                        //         subCategory: __('behaviour')
+                        //     }
+                        // });
                     }
                 });
             }
+        },
 
-            testRunner.on('destroy', function () {
-                $(window).off('.' + this.name);
-            });
+        destroy: function destroy() {
+            $(window).off('.' + this.getName());
+            document.removeEventListener('copy', handleCopyEvent);
         }
     });
 });
