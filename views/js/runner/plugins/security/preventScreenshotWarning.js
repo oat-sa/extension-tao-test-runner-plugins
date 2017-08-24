cc7a363c98874a241ac8669410485f8a5538f73b
Aleksej Tikhanovich <aleksej@taotesting.com>
2017-08-24 17:26:31 +0300

    Added new plugins for blur and screenshots with only warnings

---
+++ views/js/runner/plugins/security/preventScreenshotWarning.js
@@ -0,0 +1,136 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA ;
+ */
+
+define([
+    'jquery',
+    'i18n',
+    'taoTests/runner/plugin'
+], function ($, __, pluginFactory) {
+    'use strict';
+
+    /**
+     * Identify the current platform
+     * @type {String}
+     */
+    var platform = navigator.platform.indexOf('Mac') < 0 ? 'win' : 'mac';
+
+    /**
+     * Sniff Internet Explorer which is the only browser to implement window.clipboardData
+     * @type {Boolean}
+     */
+    var isIe = typeof window.clipboardData !== 'undefined';
+
+    /**
+     * On windows, this is what we will attempt to put in the clipboard on a copy event
+     * @type {String}
+     */
+    var overrideContent = ' ';
+
+
+    function triggerCopyEvent() {
+        if (isIe) {
+            document.designMode = 'on'; // IE won't actually trigger the 'copy' event without this
+        }
+        if (document.execCommand && document.queryCommandSupported('copy')) {
+            document.execCommand('copy');
+        }
+        if (isIe) {
+            document.designMode = 'off';
+        }
+    }
+
+    function handleCopyEvent(event) {
+        overrideClipboard(event.clipboardData);
+        event.preventDefault();
+    }
+
+    function overrideClipboard(clipboardData) {
+        if (isIe) {
+            window.clipboardData.setData('Text', overrideContent);
+        } else {
+            clipboardData.setData('text/plain', overrideContent);
+        }
+    }
+
+    /**
+     * Creates the plugin.
+     * Prevents screenshots (mac) and pauses assessment on print screen (win)
+     */
+    return pluginFactory({
+
+        name: 'preventScreenshotWarning',
+
+        /**
+         * Initializes the plugin (called during runner's init)
+         */
+        init: function init() {
+            // this function is mandatory
+        },
+
+        /**
+         * Called when the host is installing the plugins
+         * @returns {Promise} to resolve async delegation
+         */
+        install: function install() {
+            var testRunner = this.getTestRunner();
+            if (!('preventScreenshot' in testRunner.getPlugins())) {
+                // For mac - blur on Cmd+Shift
+                if (platform === 'mac') {
+                    $(window)
+                        .on('keydown' + '.' + this.getName(), function (e) {
+                            if (e.metaKey && e.shiftKey) {
+                                $('body').css('filter', 'blur(20px)');
+                            }
+                        })
+                        // Note - When user hits Cmd+Shift+4, they must press any key
+                        // to remove blur (that is not Cmd+Shift)
+                        .on('keyup' + '.' + this.getName(), function (e) {
+                            if (!e.metaKey || !e.shiftKey) {
+                                $('body').css('filter', '');
+                            }
+                        });
+                }
+
+                // Windows - pause on PrtScn
+                else if (platform === 'win') {
+                    // will override, if possible, anything put into the clipboard after a copy event (whether manually or automatically triggered)
+                    document.addEventListener('copy', handleCopyEvent);
+
+                    $(window)
+                        .on('keyup' + '.' + this.getName(), function (e) {
+                            if (e.key === 'PrintScreen') {
+                                triggerCopyEvent();
+
+                                testRunner
+                                    .trigger('prohibited-key', 'PrintScreen')
+                                    .trigger('log', __('Attempt to print screen. Please contact your proctor or administrator to resume your assessment.'));
+                            }
+                        });
+                }
+            }
+
+        },
+
+        destroy: function destroy() {
+            $(window).off('.' + this.getName());
+            if (platform === 'win') {
+                document.removeEventListener('copy', handleCopyEvent);
+            }
+        }
+    });
+});
