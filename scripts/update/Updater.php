f43c5b5ddb45e635dd32dca04cd83b5e4841a8c3
Tamas Besenyei <tamas@besenyei.net>
2019-08-26 15:43:07 +0200

    version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -264,6 +264,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('2.6.0');
         }
 
-        $this->skip('2.6.0', '2.9.0');
+        $this->skip('2.6.0', '2.9.1');
     }
 }
