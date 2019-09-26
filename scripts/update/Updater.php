ba9dc24e2bae4b78424ecfd260dbd3bb9f322eba
Aleh Hutnikau <hutnikau@1pt.com>
2019-09-26 23:06:07 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -264,6 +264,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('2.6.0');
         }
 
-        $this->skip('2.6.0', '2.6.0.2');
+        $this->skip('2.6.0', '2.6.0.3');
     }
 }
