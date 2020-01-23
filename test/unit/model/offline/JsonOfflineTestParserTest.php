1ecebe36942cfe4e0a3a2b6f6458b16ca9415e3b
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2020-01-23 10:39:38 +0300

    Unit tests

--- test/unit/model/offline/JsonOfflineTestParserTest.php
+++ test/unit/model/offline/JsonOfflineTestParserTest.php
@@ -19,12 +19,27 @@
 
 namespace oat\taoTestRunnerPlugins\test\unit\model\offline;
 
+use common_Exception;
+use oat\generis\model\kernel\uri\UriProvider;
 use oat\generis\test\TestCase;
 use oat\oatbox\filesystem\File;
 use oat\taoTestRunnerPlugins\model\offline\JsonOfflineTestParser;
+use PHPUnit_Framework_MockObject_MockObject;
 
 class JsonOfflineTestParserTest extends TestCase
 {
+    /** @var PHPUnit_Framework_MockObject_MockObject|File */
+    private $fileMock;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->fileMock = $this->getMockBuilder(File::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
     /**
      * @dataProvider isInterruptedDataProvider
      *
@@ -33,17 +48,114 @@ class JsonOfflineTestParserTest extends TestCase
      */
     public function testIsInterrupted($json, $expected)
     {
-        /** @var File $file */
-        $file = $this->getMockBuilder(File::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $file->method('read')
+        $this->fileMock->method('read')
             ->willReturn($json);
 
-        $parser = new JsonOfflineTestParser($file);
+        $parser = new JsonOfflineTestParser($this->fileMock);
 
         $this->assertEquals($parser->isInterrupted(), $expected);
+    }
+
+    public function testCheckUri()
+    {
+        $this->fileMock->method('read')
+            ->willReturn('{}');
+
+        $parser = new JsonOfflineTestParser($this->fileMock);
+        $parser->setServiceLocator($this->getMockServiceLocator());
+
+        $this->assertTrue($parser->checkUri('http://local.domain/first.rdf#i1544535042652725'));
+        $this->assertNotTrue($parser->checkUri('http://local/first.rdf#i1544535042652725'));
+    }
+
+    public function testValidateUrisAllCorrect()
+    {
+        $json = '{
+          "testConfig": {
+            "exitUrl": "http://local.domain/taoDelivery/DeliveryServer/index",
+            "testDefinition": "http://local.domain/first.rdf#i1544535042652725",
+            "testCompilation": "http://local.domain/first.rdf#i1544535107181528-|http://sample/first.rdf#i1544535107965629+",
+            "serviceCallId": "http://local.domain/first.rdf#i154661273822140"
+          },
+          "actionQueue": [
+            {
+              "parameters": {
+                "testDefinition": "http://local.domain/first.rdf#i1544535042652725",
+                "testCompilation": "http://local.domain/first.rdf#i1544535107181528-|http://sample/first.rdf#i1544535107965629+",
+                "serviceCallId": "http://local.domain/first.rdf#i154661273822140"
+              }
+            }
+          ]
+        }';
+
+        $this->fileMock->method('read')
+            ->willReturn($json);
+
+        $parser = new JsonOfflineTestParser($this->fileMock);
+        $parser->setServiceLocator($this->getMockServiceLocator());
+
+        $this->assertTrue($parser->validateUris());
+
+    }
+
+    public function testValidateUrisSeveralIncorrect()
+    {
+        $json = '{
+          "testConfig": {
+            "exitUrl": "http://sample.local/taoDelivery/DeliveryServer/index",
+            "testDefinition": "http://sample.local/first.rdf#i1544535042652725",
+            "testCompilation": "http://sample.local/first.rdf#i1544535107181528-|http://sample/first.rdf#i1544535107965629+",
+            "serviceCallId": "http://sample.local/first.rdf#i154661273822140"
+          },
+          "actionQueue": [
+            {
+              "parameters": {
+                "testDefinition": "http://sample.local/first.rdf#i1544535042652725",
+                "testCompilation": "http://local.domain/first.rdf#i1544535107181528-|http://sample/first.rdf#i1544535107965629+",
+                "serviceCallId": "http://sample.local/first.rdf#i154661273822140"
+              }
+            }
+          ]
+        }';
+
+        $this->fileMock->method('read')
+            ->willReturn($json);
+
+        $parser = new JsonOfflineTestParser($this->fileMock);
+        $parser->setServiceLocator($this->getMockServiceLocator());
+
+        $this->assertFalse($parser->validateUris());
+    }
+
+    public function testValidate()
+    {
+        $json = '{
+          "testConfig": {
+            "exitUrl": "http://sample.local/taoDelivery/DeliveryServer/index",
+            "testDefinition": "http://sample.local/first.rdf#i1544535042652725",
+            "testCompilation": "http://sample.local/first.rdf#i1544535107181528-|http://sample/first.rdf#i1544535107965629+",
+            "serviceCallId": "http://sample.local/first.rdf#i154661273822140"
+          },
+          "actionQueue": [
+            {
+              "parameters": {
+                "testDefinition": "http://sample.local/first.rdf#i1544535042652725",
+                "testCompilation": "http://local.domain/first.rdf#i1544535107181528-|http://sample/first.rdf#i1544535107965629+",
+                "serviceCallId": "http://sample.local/first.rdf#i154661273822140"
+              }
+            }
+          ]
+        }';
+
+        $this->fileMock->method('read')
+            ->willReturn($json);
+
+        $parser = new JsonOfflineTestParser($this->fileMock);
+        $parser->setServiceLocator($this->getMockServiceLocator());
+
+        $this->expectException(common_Exception::class);
+
+        $parser->validate();
     }
 
     public function isInterruptedDataProvider()
@@ -58,5 +170,15 @@ class JsonOfflineTestParserTest extends TestCase
             ['{"isExitTest":1}', true],
             ['{"isExitTest":0}', false],
         ];
+    }
+
+    private function getMockServiceLocator()
+    {
+        $uriProvider = $this->prophesize(UriProvider::class);
+        $uriProvider->provide()->willReturn('http://local.domain/first.rdf#i1544535042650000');
+
+        return $this->getServiceLocatorMock([
+            UriProvider::SERVICE_ID => $uriProvider
+        ]);
     }
 }
