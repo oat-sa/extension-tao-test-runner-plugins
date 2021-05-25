f591e2d9549c110bb2508df4774bcbce3369dde0
lecosson <Alpha6031769>
2021-05-25 15:40:51 +0300

    fix: TR-1197 tune window gap size

--- views/js/runner/plugins/security/fullScreen.js
+++ views/js/runner/plugins/security/fullScreen.js
@@ -123,15 +123,16 @@ define([
      * @returns {Boolean}
      */
     function checkFullScreen() {
-        const screenSizeGap = 30;
+
         if (fullScreenProperty in doc) {
             const isGenericFullScreen = !!doc[fullScreenProperty];
-            const isSizeFullScreen = Math.abs (screen.width - window.outerWidth) < screenSizeGap
-                && Math.abs (screen.height - window.outerHeight) < screenSizeGap;
+            const screenSizeGap = 16;
+            const isSizeFullScreen = Math.abs (screen.width - window.outerWidth) <= screenSizeGap
+                && Math.abs (screen.height - window.outerHeight) <= screenSizeGap;
             return isGenericFullScreen || isSizeFullScreen;
         } else {
             // when the browser does not implement the full screen API, arbitrary checks if the full screen mode is active
-            return (screen.availHeight || screen.height - screenSizeGap) <= window.innerHeight;
+            return (screen.availHeight || screen.height - 30) <= window.innerHeight;
         }
     }
 
